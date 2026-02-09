a1118410592cbddee19b50b279fc203f5eee888b
Antoni <Antoni>
2026-02-09 16:46:31 +0100

    primer commit

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+
+    public function store(Request $request)
+    {
+        try {
+            // Validació amb missatges personalitzats
+            $validated = $request->validate([
+                   'email'     => 'required|string|email',
+                    'password' => 'required|string',
+                ],[ 'email.required' => 'El correu és obligatori',
+                    'email.email'    => 'El correu no té un format vàlid',
+                    'password.required' => 'La contrasenyaa és obligatòria',
+                ]
+            );
+            // Intent d'inici de sessió
+            if (!Auth::attempt($validated)) {
+                return response()->json([
+                    'message' => 'Credencials d\'accés invàlides'
+                ], 401);
+            }
+
+            // Usuari autenticat
+            $user = Auth::guard('sanctum')->user();
+
+            // Crear token d'accés
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            // Resposta JSON
+            return response()->json([
+                'access_token' => $token,
+                'token_type'   => 'Bearer',
+                'user'         => $user,
+                'status'       => 'Login OK successful',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'S\'ha produït un error al tractar les dades',
+                'error_details' => $e->getMessage(),
+            ], 200);
+        }
+    }
+
+    public function destroy(Request $request)
+    {
+        try {
+            $token = Auth::guard('sanctum')->user()->currentAccessToken();
+            $token->delete();
+
+            return response()->json(['message' => 'Logout OK successful']);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'S\'ha produït un error al tractar les dades',
+                'error_details' => $e->getMessage(),
+            ], 200);
+        }
+    }
+}
